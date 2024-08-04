129eae57a5572c3fee618958d71ae93e17b33e4b
Ujjwal2203 <ujjwaljain8447@gmail.com>
2024-08-04 23:35:35 +0530

    cloudinary file upload

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer  from "multer";
+
+// multer is used for uploading files into the local storage temporary then these files will be uploaded to cloudnary ]]
+
+// in multer we using diskstorage to store files in disk there are other options available such as - memory storage etc , but storing files in memiry is not right 
+
+const storage = multer.diskStorage({
+  destination: function(req,file,cb){  // req comes from user and can be processed in express but file cann't be processed in express that's why multer is used  and cb is nothing but a callback funtion 
+    cb(null , './public/temp')
+  },
+  filename: function(req,file,cb){
+    cb(null,file.originalname) // file has many methods read documentation
+  }
+})
+
+export const upload = multer({
+  storage
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+ });
+  
+const uploadoncloudinary = async (localfilepath) => {
+  try {
+    if(!localfilepath){
+      console.log("error occured could't find the localfilepath")
+     return null 
+
+    }
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localfilepath,{
+      resource_type: "auto"
+     })
+     // file has been uploaded successfully 
+     console.log("file uploaded on cloudinary ", response.url)
+     return response;
+  } catch (error) {
+    fs.unlinkSync(localfilepath) // remove the locally saved temporary  file as operation fails 
+    return null; 
+    
+  }
+}
+
+export {uploadoncloudinary}
